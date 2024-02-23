54aeb7a4a4c5626ac9ea5ef2644595aed9d6ae2a
Muhammad Abdullah <201617abdullah@gmail.com>
2024-02-23 17:59:11 +0500

    Category.cs file added

---
+++ FirstApp/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstApp.Models
+{
+    public class Category
+    { //key for defining that Id is the primary key here
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
